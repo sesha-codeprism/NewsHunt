04c5a8d559514bf06c8a6c47ccbee761c68829b9
sesha-codeprism <sesha.singaraju@codeprism.in>
2020-05-12 01:43:49 +0530

    React native gesture handler tap fix

--- android/app/src/main/java/com/newshunt/MainActivity.java
+++ android/app/src/main/java/com/newshunt/MainActivity.java
@@ -1,6 +1,13 @@
 package com.newshunt;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.content.Intent; // <--- import
+import com.airbnb.android.react.lottie.LottiePackage;
+import android.content.res.Configuration; // <--- import
+
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +18,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "NewsHunt";
+  }
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
